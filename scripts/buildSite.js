6372f9b7d2c9170b26e2754dd211f5231024817d
Ian Macalinao <ian@saber.so>
2021-10-01 22:17:47 -0400

    rebuild

--- scripts/buildSite.js
+++ scripts/buildSite.js
@@ -2,16 +2,13 @@ const fs = require("fs/promises");
 const info = require("../data/distributor-info.json");
 
 const main = async () => {
-  await fs.rm("site/", { recursive: true });
-  await fs.mkdir("site/proofs/", { recursive: true });
-  await fs.copyFile("index.html", "site/index.html");
-  await fs.copyFile("data/airdrop-amounts.csv", "site/airdrop-amounts.csv");
-  await fs.copyFile("data/distributor-info.json", "site/distributor-info.json");
+  await fs.rm("proofs/", { recursive: true });
+  await fs.mkdir("proofs/", { recursive: true });
   await Promise.all(
     info.claims.map(async (claim) => {
       await Promise.all(
         Object.entries(claim).map(async ([k, v]) => {
-          await fs.writeFile(`site/proofs/${k}.json`, JSON.stringify(v));
+          await fs.writeFile(`proofs/${k}.json`, JSON.stringify(v));
         })
       );
     })
